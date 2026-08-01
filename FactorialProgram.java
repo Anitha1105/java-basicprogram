d17b4c11ddb8d0ccd2724ff320ae76bb7263ccb4
Anitha1105 <anithabeeshu2022@gmail.com>
2026-08-01 15:34:48 +0530

    Update FactorialProgram.java
    Variable name changed as Factorial

--- FactorialProgram.java
+++ FactorialProgram.java
@@ -3,9 +3,9 @@ package basicjavaprogram;
 public class FactorialProgram {
 
 	public static void main(String[] args) {
-		// Factorial Program
-		int factNumber=5;
-		long factValue=1;
+		// Factorial Program--Changes
+		int factorialNumber=5;
+		long factorialValue=1;
 		for(int i=factNumber;i>=1;i--) {
 			factValue=factValue*i;
 		}
