ffb539cedc2f8de97c20040aa67b1a1e3a055edd
BeeshmaBP <bheeshmachariyar@gmail.com>
2026-08-01 15:08:11 +0530

    Implement additional string reversal method
    Added an alternative method to reverse a string using a different approach.

--- BasicProgram.java
+++ BasicProgram.java
@@ -9,6 +9,19 @@ public class BasicProgram {
 		for(int i=revArr.length-1;i>=0;i--) {
 			System.out.print(revArr[i]+" ");
 		}
+			// Another way of reverse string
+		String textInput="Selenium is an automation tool";
+	    String stringArr[]=textInput.split("\\s+");
+	    String reverseString="";
+	
+	
+	for(int j=stringArr.length-1;j>=0;j--)
+	{
+		reverseString=reverseString+stringArr[j]+" ";
+		
+	}
+	System.out.println();
+	System.out.println(reverseString);
 
 
 
