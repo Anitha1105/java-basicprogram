cedc937e2911626245bacdbcc2c707f0d5a77a15
Anitha1105 <anithabeeshu2022@gmail.com>
2026-08-01 15:56:10 +0530

    Data type changed

--- FactorialProgram.java
+++ FactorialProgram.java
@@ -4,8 +4,8 @@ public class FactorialProgram {
 
 	public static void main(String[] args) {
 		// Factorial Program--Changes
-		int factorialNumber=5;
-		long factorialValue=1;
+		float factorialNumber=112;
+		double factorialValue=1;
 		for(int i=factorialNumber;i>=1;i--) {
 			factorialValue=factValue*i;
 		}
